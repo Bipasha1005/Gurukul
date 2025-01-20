28ee8e77aff431a6069f4998efb05783dfd8c91e
Bipasha_Acharjee <118049787+Bipasha1005@users.noreply.github.com>
2025-01-20 19:18:30 +0530

    Add files via upload

---
+++ js/admin_script.js
@@ -0,0 +1,16 @@
+let body = document.body;
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.header .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
---
+++ js/script.js
@@ -0,0 +1,25 @@
+let body = document.body;
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   searchForm.classList.remove('active');
+}
+
+document.querySelectorAll('input[type="number"]').forEach(InputNumber => {
+   InputNumber.oninput = () =>{
+      if(InputNumber.value.length > InputNumber.maxLength) InputNumber.value = InputNumber.value.slice(0, InputNumber.maxLength);
+   }
+});
+
+window.onscroll = () =>{
+   profile.classList.remove('active');
+   searchForm.classList.remove('active');
+
+   if(window.innerWidth < 1200){
+      sideBar.classList.remove('active');
+      body.classList.remove('active');
+   }
+
+}
